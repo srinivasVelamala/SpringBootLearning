96f790545b5dd6958ea098693a3350578b63dac8
srinivasv18 <srinivasv18@gmail.com>
2024-06-29 17:00:03 +0530

    kafka sample

---
+++ springboot.kafka.tutorial/src/test/java/myapps/kafkasample/springboot/kafka/tutorial/ApplicationTests.java
@@ -0,0 +1,13 @@
+package myapps.kafkasample.springboot.kafka.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
